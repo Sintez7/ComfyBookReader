351107435783ecc93df6b15fdef5732ac4bb21d1
Daniil <berzzzurgg@gmail.com>
2021-12-12 22:56:42 +0300

    work 12 12

---
+++ src/main/java/com/cbr/cbr/ConsoleReader.java
@@ -0,0 +1,38 @@
+package com.cbr.cbr;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ConsoleReader {
+
+    @PostConstruct
+    private void init() {
+        File sample = new File("src/main/resources/static/sample.fb2");
+        System.out.println("Sample can read?: " + sample.canRead());
+        System.out.println("path: " + sample.getAbsolutePath());
+
+        List<String> sampleStrings = new ArrayList<>();
+
+        if (sample.canRead()) {
+            try {
+                sampleStrings = Files.readAllLines(Path.of(sample.getPath()), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+//
+//        for (String sampleString : sampleStrings) {
+//            System.out.println(sampleString);
+//        }
+    }
+}
---
+++ src/main/java/com/cbr/cbr/controller/SimpleViewController.java
@@ -0,0 +1,11 @@
+package com.cbr.cbr.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleViewController {
+
+    public void readDoc() {
+
+    }
+}
