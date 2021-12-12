7dbac1e2eab9e7bcd3bf980d352e754205108f23
Daniil <berzzzurgg@gmail.com>
2021-12-12 14:53:25 +0300

    initial commit

---
+++ src/test/java/com/cbr/cbr/CbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbr.cbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
