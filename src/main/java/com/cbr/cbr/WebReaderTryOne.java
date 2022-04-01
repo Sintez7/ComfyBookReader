3e46812c749a57828c773e2332679fef5aeeefd2
Sintez_7 <berzzzurgg@gmail.com>
2022-04-01 17:29:29 +0300

    work 01 04

--- src/main/java/com/cbr/cbr/WebReaderTryOne.java
+++ src/main/java/com/cbr/cbr/WebReaderTryOne.java
@@ -6,6 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Controller
 @RequestMapping("/")
@@ -13,10 +21,22 @@ public class WebReaderTryOne {
 
     @GetMapping
     public String method1(Model model) {
-        System.err.println(model);
-        model.addAttribute("att1", "att1val");
-        model.addAttribute("objattadd");
-        System.err.println(model);
-        return "testing1";
+        File sample = new File("src/main/resources/static/sample.fb2");
+        System.out.println("Sample can read?: " + sample.canRead());
+        System.out.println("path: " + sample.getAbsolutePath());
+
+        List<String> sampleStrings = new ArrayList<>();
+
+        if (sample.canRead()) {
+            try {
+                sampleStrings = Files.readAllLines(Path.of(sample.getPath()), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        model.addAttribute("sampleLines", sampleStrings);
+
+        return "testing2";
     }
 }
